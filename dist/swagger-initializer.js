b3f3dce3ae08f9553285d4d2fb44e3873ed5a109
mf62607 <148009928+mf62607@users.noreply.github.com>
2024-03-18 03:29:37 +0000

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://idratherbewriting.com/ docs/openapi_spec_and_generated_ref_docs/openapi_openweathermap.yml", 
+    url: "http://petstore.swagger.io/v2/swagger.json", 
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
